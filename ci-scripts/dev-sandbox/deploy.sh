7f4b1d1f3de98e0dba280c6c68e40c178e10628c
Pavel Macík <pavel.macik@gmail.com>
2024-10-11 14:53:30 +0200

    chore(RHIDP-3149): prepare for devsandbox test
    Signed-off-by: Pavel Macík <pavel.macik@gmail.com>

--- ci-scripts/dev-sandbox/deploy.sh
+++ ci-scripts/dev-sandbox/deploy.sh
@@ -2,16 +2,19 @@
 
 export RHDH_OPERATOR_NAMESPACE=rhdh-operator
 
+export GITHUB_TOKEN GITHUB_USER GITHUB_REPO QUAY_TOKEN KUBECONFIG PRE_LOAD_DB
+
+GITHUB_TOKEN=$(cat /usr/local/ci-secrets/backstage-performance/github.token)
+GITHUB_USER=$(cat /usr/local/ci-secrets/backstage-performance/github.user)
+GITHUB_REPO=$(cat /usr/local/ci-secrets/backstage-performance/github.repo)
+QUAY_TOKEN=$(cat /usr/local/ci-secrets/backstage-performance/quay.token)
+
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck disable=SC1090,SC1091
 source "$(readlink -m "$SCRIPT_DIR/../../test.env")"
 
 rootdir=$(readlink -m "$SCRIPT_DIR/../..")
 
-cli="oc"
-clin="$cli -n $RHDH_OPERATOR_NAMESPACE"
-
-$cli create namespace "$RHDH_OPERATOR_NAMESPACE" --dry-run=client -o yaml | $cli apply -f -
-
-until envsubst <"$rootdir/ci-scripts/rhdh-setup/template/backstage/secret-rhdh-pull-secret.yaml" | $clin apply -f -; do $clin delete secret rhdh-pull-secret --ignore-not-found=true; done
-envsubst <"$SCRIPT_DIR/operator.yaml" | $cli apply -f -
+pushd "$rootdir"
+make ci-deploy
+popd
--- ci-scripts/dev-sandbox/undeploy.sh
+++ ci-scripts/dev-sandbox/undeploy.sh
@@ -2,12 +2,19 @@
 
 export RHDH_OPERATOR_NAMESPACE=rhdh-operator
 
+export GITHUB_TOKEN GITHUB_USER GITHUB_REPO QUAY_TOKEN KUBECONFIG PRE_LOAD_DB
+
+GITHUB_TOKEN=$(cat /usr/local/ci-secrets/backstage-performance/github.token)
+GITHUB_USER=$(cat /usr/local/ci-secrets/backstage-performance/github.user)
+GITHUB_REPO=$(cat /usr/local/ci-secrets/backstage-performance/github.repo)
+QUAY_TOKEN=$(cat /usr/local/ci-secrets/backstage-performance/quay.token)
+
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck disable=SC1090,SC1091
 source "$(readlink -m "$SCRIPT_DIR/../../test.env")"
 
-cli="oc"
+rootdir=$(readlink -m "$SCRIPT_DIR/../..")
 
-$cli delete namespace "$RHDH_OPERATOR_NAMESPACE" --ignore-not-found=true
-
-envsubst <"$SCRIPT_DIR/operator.yaml" | $cli delete -f - --ignore-not-found=true
+pushd "$rootdir"
+make clean-all
+popd
